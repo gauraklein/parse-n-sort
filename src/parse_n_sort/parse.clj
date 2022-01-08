af93d6bb2fc17234a715c8731330a389af2ebb48
gauraklein <gaurakklein@gmail.com>
2022-01-08 00:22:40 -0600

    added function to consume input file and return a vector of vectors for each line

---
+++ src/parse_n_sort/parse.clj
@@ -0,0 +1,21 @@
+(ns parse-n-sort.parse
+  (:require
+   [clojure.string :as str]))
+
+(defn parse-file
+  "Takes a string and splits based on delimiter returns a vector"
+  [file]
+  {:pre [(string? file)]}
+  (let [delimiter  (some
+                    #(when (re-find % file) %)
+                    [#" \| " #", " #" "])]
+    (if
+     delimiter
+      (->> file
+           (str/split-lines)
+           (map #(str/split % delimiter))
+           (map #(zipmap [:last-name :first-name :email :color :birth-date] %))
+           (vec))
+      "Unable to find a valid delimiter, please check your file and try again")))
+
+;; (parse-file (slurp "./resources/pipe.txt"))
--- test/parse_n_sort/core_test.clj
+++ test/parse_n_sort/core_test.clj
@@ -1,7 +1,29 @@
 (ns parse-n-sort.core-test
   (:require [clojure.test :refer :all]
-            [parse-n-sort.core :refer :all]))
+            [parse-n-sort.parse :refer [parse-file]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; parse-file
+
+(def pipe-str "last-name-1 | first-name-1 | email-1 | color-1 | birth-date-1\nlast-name-2 | first-name-2 | email-2 | color-2 | birth-date-2")
+(def comma-str "last-name-1, first-name-1, email-1, color-1, birth-date-1\nlast-name-2, first-name-2, email-2, color-2, birth-date-2")
+(def space-str "last-name-1 first-name-1 email-1 color-1 birth-date-1\nlast-name-2 first-name-2 email-2 color-2 birth-date-2")
+
+(def correct-output [{:last-name "last-name-1", :first-name "first-name-1", :email "email-1", :color "color-1", :birth-date "birth-date-1"}
+                     {:last-name "last-name-2", :first-name "first-name-2", :email "email-2", :color "color-2", :birth-date "birth-date-2"}])
+
+(deftest parse-file-test
+
+  (testing "throws an exception when arg is not a string"
+    (is (thrown? java.lang.AssertionError (parse-file 1)))
+    (is (thrown? java.lang.AssertionError (parse-file nil)))
+    (is (thrown? java.lang.AssertionError (parse-file correct-output))))
+
+  (testing "returns a message when no valid delimiter is found"
+    (is (= "Unable to find a valid delimiter, please check your file and try again" (parse-file ""))))
+
+  (testing "returns the correct output for properly formatted files"
+    (is (= correct-output (parse-file pipe-str)))
+    (is (= correct-output (parse-file comma-str)))
+    (is (= correct-output (parse-file space-str)))))
+
+  (run-tests)
