61592e311f12f4cd8e69a6586f6ecfda0b10dceb
gauraklein <gaurakklein@gmail.com>
2022-01-07 22:01:05 -0600

    init

---
+++ src/parse_n_sort/core.clj
@@ -0,0 +1,7 @@
+(ns parse-n-sort.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/parse_n_sort/core_test.clj
@@ -0,0 +1,7 @@
+(ns parse-n-sort.core-test
+  (:require [clojure.test :refer :all]
+            [parse-n-sort.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
